7b5956975284c82187b329d232352ea2b96c2f0b
TyHulet <tyhulet@byui.edu>
2024-04-30 13:18:15 -0600

    Finshed Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number? ");
+        //int magicnumber = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int magicnumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicnumber)
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicnumber > guess)
+            {
+                Console.WriteLine("Higher");
+
+            }
+            else if (magicnumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine($"You guessed it!");
+            }
+        }
     }
+
 }
