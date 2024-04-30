3b8c3359367972068011b92333080a63f1e73e80
TyHulet <tyhulet@byui.edu>
2024-04-30 13:37:29 -0600

    Finshed Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int usernumber = -1;
+        while (usernumber != 0)
+        {
+            Console.Write("Enter a number (0 to quit): ");
+
+            string userresponse = Console.ReadLine();
+            usernumber = int.Parse(userresponse);
+
+            if (usernumber != 0)
+            {
+                numbers.Add(usernumber);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The Sum is: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The Average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The Max is: {max}");
     }
 }
