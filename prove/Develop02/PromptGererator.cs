c6b9d6ff3711435b47739f633a6f1040af40c849
TyHulet <tyhulet@byui.edu>
2024-05-17 11:48:12 -0600

    fixed the PromptGererator file

--- prove/Develop02/PromptGererator.cs
+++ prove/Develop02/PromptGererator.cs
@@ -1,19 +1,18 @@
 public class PromptGererator
 {
-    public List<string> Randomprompt = new List<string>(){
+    public static List<string> prompt = new List<string>(){
         "What was the strongest emotion I felt today?",
         "What was the best part of my day?",
         "Did anything interesting happen at work to day",
         "Did I meet anyone interesting today?",
         "Did anyting happen today that I need to refelect on?",
         "What did i learn about in my scriptures studys today?"
-
     };
 
-    public string RandomPrompt()
+    public static string RandomPrompt()
     {
         Random random = new Random();
-        int index = random.Next(Randomprompt.Count);
-        return Randomprompt[index];
+        int index = random.Next(prompt.Count);
+        return prompt[index];
     }
 }
