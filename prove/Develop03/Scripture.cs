87b9cc19f9cef7ba09d531ceb0bb13ff6da7677c
TyHulet <tyhulet@byui.edu>
2024-05-30 13:15:14 -0600

    addded in the base starting of my memorsier code

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,28 @@
+using System.ComponentModel;
+
+public class Scripture
+{
+    private Reference _reference;
+
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+
+    public string DisplayText()
+    {
+        return "";
+    }
+
+    public void IsCompletelyHidden()
+    {
+
+    }
+}
