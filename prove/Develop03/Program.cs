bb35e70eb6ff59dc201d25c1aa31dc39db69d09c
TyHulet <tyhulet@byui.edu>
2024-05-31 19:01:05 -0600

    scripture memorizer working

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Net.Quic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        //string reference = "John 3:16";
+        //string text = "For God so loved the world, that He gave His only begotten Son, that whosever believeth in Him should not perish, but have everlasting life.";
+        Reference reference = new Reference("John", 3, 16);
+        Scripture scripture = new Scripture(reference, "For God so loved the world, that He gave His only begotten Son, that whosever believeth in Him should not perish, but have everlasting life.");
+        bool quit = false;
+        while (!scripture.IsCompletelyHidden() && !quit)
+        {
+            Console.Clear();
+            Console.WriteLine("Scripture Memorizer");
+            scripture.Displaytext();
+
+            Console.Write("Press Enter to hide a word of type 'quit to exit: ");
+            string userInput = Console.ReadLine().ToLower();
+
+            if (userInput == "quit")
+            {
+                quit = true;
+            }
+            else
+            {
+                scripture.HideRandomWords();
+            }
+        }
+        if (quit)
+        {
+            Console.WriteLine("Leaving so soon!");
+        }
+        else
+        {
+            Console.Clear();
+            scripture.Displaytext();
+            Console.WriteLine("Great job, you memorized the scripture!");
+        }
+
     }
 }
