1e89bebdbd910fb4c379fe5fd781bdb1ebee536c
TyHulet <tyhulet@byui.edu>
2024-06-13 19:03:24 -0600

    added in the menu options for the programs

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        var choice = "";
+        do
+        {
+            Console.WriteLine("Actvity Options: ");
+            Console.WriteLine(" 1. Begin Breathing Actvity");
+            Console.WriteLine(" 2. Begin Reflecting Activity");
+            Console.WriteLine(" 3. Begin Listing Activity");
+            Console.WriteLine(" 4. Quit");
+            Console.WriteLine("Please Select an actvity you would like to start.");
+            choice = Console.ReadLine();
+            if (choice == "1")
+            {
+
+            }
+            else if (choice == "2")
+            {
+
+            }
+            else if (choice == "3")
+            {
+
+            }
+            else if (choice == "5")
+            {
+                Console.WriteLine("Goodbye");
+            }
+            else
+
+            {
+                Console.WriteLine("Invalid Choice");
+            }
+            Console.WriteLine();
+
+        }
+        while (choice != "4");
     }
 }
