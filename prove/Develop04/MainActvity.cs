7be36c0c41bbd2ca1294276af08488e49c979eeb
TyHulet <tyhulet@byui.edu>
2024-06-13 19:36:12 -0600

    set the main actvity program for the code

---
+++ prove/Develop04/MainActvity.cs
@@ -0,0 +1,37 @@
+public class MainActvity
+{
+    private string _name;
+
+    private string _actvityDescription;
+
+    private int _duration;
+
+    private string _endingMessage;
+
+    public void StartingActvity()
+    {
+
+    }
+
+    public void DisplayStartingMessage()
+    {
+
+    }
+
+    public void DisplayEndingMessage()
+    {
+
+    }
+
+    public void timer(int secounds)
+    {
+
+    }
+
+    public void CountdownEvent()
+    {
+
+    }
+
+}
+
