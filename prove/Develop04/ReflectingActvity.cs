3cd73cac86ac51f8a107b525e20fe962eebb134b
TyHulet <tyhulet@byui.edu>
2024-06-13 19:36:57 -0600

    updated the Reflecting Actvity with alot of errors to be fixed at a later time.

---
+++ prove/Develop04/ReflectingActvity.cs
@@ -0,0 +1,26 @@
+public class ReflectingActvity
+{
+    private List<prompts> _reflectionprompts;
+
+    private List<question> __reflectionquestions;
+
+    public void RunReflecting()
+    {
+
+    }
+
+    public string RandomPrompt()
+    {
+        return RandomPrompt;
+    }
+
+    public string RandomQuestion()
+    {
+        return RandomQuestion;
+    }
+
+    public void ShowPrompt()
+    {
+
+    }
+}
