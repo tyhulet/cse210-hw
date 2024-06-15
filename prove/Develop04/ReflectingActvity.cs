835047a2b722c78d85a804ff1ebed3c6c0bc8419
TyHulet <tyhulet@byui.edu>
2024-06-14 21:51:27 -0600

    Addedd in the prompts and questions.

--- prove/Develop04/ReflectingActvity.cs
+++ prove/Develop04/ReflectingActvity.cs
@@ -1,32 +1,20 @@
-public class ReflectingActvity
+public class ReflectingActivity : MainActvity
 {
-    private List<string> _reflectionprompts = new List<string>()
-    {
-
+    protected List<string> _reflectingprompts = new List<string>(){
+        "Think of a time when you stood up for someone else.",
+        "Think of a time when you did something really difficult.",
+        "Think of a time when you helped someone in need.",
+        "Think of a time when you did something truly selfless.",
     };
 
-    private List<string> __reflectionquestions = new List<string>()
-    {
-
+    protected List<string> _reflectingQuestions = new List<string>(){
+        "Why was this experience meaningful to you?",
+        "Have you ever done anything like this before?",
+        "How did you get started?",
+        "How did you feel when it was complete?",
+        "What made this time different than other times when you were no successful?",
+        "What did you learn about yourself through this experience?",
+        "How can you keep this experience in mind in the furure?"
     };
 
-    public void RunReflecting()
-    {
-
-    }
-
-    public string RandomPrompt()
-    {
-        return RandomPrompt;
-    }
-
-    public string RandomQuestion()
-    {
-        return RandomQuestion;
-    }
-
-    public void ShowPrompt()
-    {
-
-    }
 }
