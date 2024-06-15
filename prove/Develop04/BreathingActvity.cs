b53a3cf98dac3bfe1bc119878d00ceeff366e516
TyHulet <tyhulet@byui.edu>
2024-06-15 11:01:53 -0600

    Finshed the breathing actvity section

--- prove/Develop04/BreathingActvity.cs
+++ prove/Develop04/BreathingActvity.cs
@@ -10,13 +10,34 @@ public class BreathingActivity : MainActvity
     {
 
         DisplayStartingMessage();
+        Console.WriteLine("Let's Begain");
+        Thread.Sleep(1500);
         Console.WriteLine("Breathe In");
         CountdownEvent(6);
 
+        Console.WriteLine("Hold it for 2 secounds");
+        Thread.Sleep(2000);
+
+        Console.WriteLine("Now....");
+        Thread.Sleep(1000);
         Console.WriteLine("Breathe Out");
-        CountdownEvent(6);
+        CountdownEvent(5);
+
+        Console.WriteLine("Let's go again");
+        Thread.Sleep(1000);
+        Console.WriteLine("Breathe In");
+        CountdownEvent(5);
+
+        Console.WriteLine("Hold it for 2 secounds");
+        Thread.Sleep(2000);
+
+        Console.WriteLine("Now....");
+        Thread.Sleep(1000);
+        Console.WriteLine("Breathe Out");
+        CountdownEvent(5);
 
 
         DisplayEndingMessage();
+        Console.WriteLine("Returning you back to the menu.");
     }
 }
