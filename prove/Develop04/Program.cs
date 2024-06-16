c5df3fda71a26a97b163b7c90c13346ee5147924
TyHulet <tyhulet@byui.edu>
2024-06-15 19:51:39 -0600

    Made some spelling corrections

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -7,12 +7,12 @@ class Program
         var choice = "";
         do
         {
-            Console.WriteLine("Actvity Options: ");
-            Console.WriteLine(" 1. Begin Breathing Actvity");
-            Console.WriteLine(" 2. Begin Reflecting Activity");
-            Console.WriteLine(" 3. Begin Listing Activity");
+            Console.WriteLine("Activity Options: ");
+            Console.WriteLine(" 1. Begin Breathing Activity");
+            Console.WriteLine(" 2. Begin Reflecting Actiivity");
+            Console.WriteLine(" 3. Begin Listing Actiivity");
             Console.WriteLine(" 4. Quit");
-            Console.WriteLine("Please Select an actvity you would like to start.");
+            Console.WriteLine("Please Select an activity you would like to start.");
             choice = Console.ReadLine();
             if (choice == "1")
             {
