4be5f89809508a1628aaa7d16e0787a19649fc64
TyHulet <tyhulet@byui.edu>
2024-06-27 19:45:29 -0600

    Created Goal Tracker Class

---
+++ prove/Develop05/GoalTracker.cs
@@ -0,0 +1,49 @@
+using System.Configuration.Assemblies;
+
+public class GoalTracker
+{
+    private List<Goals> _goals = new List<Goals>();
+
+    private int _playerScore;
+
+
+    public void Start()
+    {
+
+    }
+
+    public void ShowUserInfo()
+    {
+
+    }
+
+    public void AddGoals()
+    {
+
+    }
+
+    public void ShowGoals()
+    {
+
+    }
+
+    public void ShowGoalsDetails()
+    {
+
+    }
+
+    public void LoadFile()
+    {
+
+    }
+
+    public void SaveFile()
+    {
+
+    }
+
+    public void RecordEvent()
+    {
+
+    }
+}
