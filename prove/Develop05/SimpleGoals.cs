1cedb1bcf6d3ca4b972448c82b25d2d661358dc0
TyHulet <tyhulet@byui.edu>
2024-06-29 13:54:53 -0600

    Updated the SimpleGoals section of the code

--- prove/Develop05/SimpleGoals.cs
+++ prove/Develop05/SimpleGoals.cs
@@ -1,4 +1,29 @@
-public class SimpleGoals
+public class SimpleGoals : Goals
 {
+    protected bool _goalCompleted;
+    public SimpleGoals(string goalName, string goalDescription, int points) : base(goalName, goalDescription, points)
+    {
 
+    }
+
+    public override int RecordEvent()
+    {
+        if (!_goalCompleted)
+        {
+            _goalCompleted = true;
+            return _points;
+        }
+        return 0;
+
+    }
+
+    public override bool CompletedGoal()
+    {
+        return _goalCompleted;
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"SimpleGoals: Completed: {_goalCompleted} | {_goalName} | {_goalDescription} | {_points}";
+    }
 }
