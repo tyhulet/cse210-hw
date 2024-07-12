0e0999a5ea0ff8d59e2502669ab8ec105ad5e17c
TyHulet <tyhulet@byui.edu>
2024-07-12 13:08:43 -0600

    Added in the address class for foudation 2

---
+++ final/Foundation2/Address.cs
@@ -0,0 +1,15 @@
+public class Address
+{
+    private string _streetAddress;
+    private string _cityName;
+    private string _state;
+    private string _country;
+
+    public Address(string streetAddress, string cityName, string state, string country)
+    {
+        _streetAddress = streetAddress;
+        _cityName = cityName;
+        _state = state;
+        _country = country;
+    }
+}
