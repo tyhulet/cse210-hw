3ac108ad6650f46f2e2e82a38e2a0121f00ae7d7
TyHulet <tyhulet@byui.edu>
2024-07-12 13:09:01 -0600

    Added in the Customer class for foudation 2

---
+++ final/Foundation2/Customer.cs
@@ -0,0 +1,11 @@
+public class Customer
+{
+    private string _customerName;
+    private Address _custromerAddress;
+
+    public Customer(string Name, Address address)
+    {
+        _customerName = Name;
+        _custromerAddress = address;
+    }
+}
