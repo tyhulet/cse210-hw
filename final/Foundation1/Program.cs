a0a0b486cc0ee911b1c46a62446b7f5afe168d90
TyHulet <tyhulet@byui.edu>
2024-07-12 11:18:33 -0600

    updated the program section

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        List<Videos> videos = new List<Videos>();
+
+        Videos video1 = new Videos("NFL's Greates Hits of the year 2024", "NFLCentral", 480);
+        Comments comment1 = new Comments("Nflfan09", "That hit by demarcus lawarnce was huge.");
+        Comments comment2 = new Comments("49'sFan", "Some great plays by my 49's");
+        Comments comment3 = new Comments("SportsFan27", "Another greart year of nfl football");
+
+        video1.AddComment(comment1);
+        video1.AddComment(comment2);
+        video1.AddComment(comment3);
+
+        videos.Add(video1);
+
+        foreach (Videos video in videos)
+        {
+            video.DisplayVideoInfo();
+        }
     }
 }
