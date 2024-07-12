5456d79df206cf6bea83e513a4ef18a27fcb8dbb
TyHulet <tyhulet@byui.edu>
2024-07-12 11:18:04 -0600

    added and updated the video class

---
+++ final/Foundation1/videos.cs
@@ -0,0 +1,43 @@
+using System.Runtime.InteropServices;
+using System.Transactions;
+
+public class Videos
+{
+    public string _videoTitle;
+
+    public string _videoAuthor;
+
+    public int _videoLength;
+
+    public List<Comments> _comment = new List<Comments>();
+
+    public Videos(string videoTitle, string videoAuthor, int videoLength)
+    {
+        _videoTitle = videoTitle;
+        _videoAuthor = videoAuthor;
+        _videoLength = videoLength;
+    }
+
+    public int commentsCounter()
+    {
+        return _comment.Count;
+    }
+
+    public void AddComment(Comments comment)
+    {
+        _comment.Add(comment);
+    }
+
+    public void DisplayVideoInfo()
+    {
+        Console.WriteLine($"Video Title: {_videoTitle}");
+        Console.WriteLine($"Video Author: {_videoAuthor}");
+        Console.WriteLine($"Video Length in Secounds: {_videoLength}");
+        Console.WriteLine($"Totla number of comments: {commentsCounter}");
+
+        foreach (Comments comment in _comment)
+        {
+            comment.DisplayComments();
+        }
+    }
+}
