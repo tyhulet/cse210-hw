7a674522fa0d685af16f2e28612e1258bdcde290
TyHulet <tyhulet@byui.edu>
2024-07-18 19:06:59 -0600

    added in the next parts for the foudation 3

---
+++ final/Foundation3/Address.cs
@@ -0,0 +1,25 @@
+public class Address
+{
+    private string _streetAddress;
+    private string _cityName;
+    private string _state;
+    private string _country;
+
+    public Address(string streetAddress, string cityName, string state, string country)
+    {
+        _streetAddress = streetAddress;
+        _cityName = cityName;
+        _state = state;
+        _country = country;
+    }
+
+    public bool OutsideUsa()
+    {
+        return _country == "usa";
+    }
+
+    public string DisplayAddress()
+    {
+        return $"{_streetAddress}, {_cityName}, {_state}, {_country}";
+    }
+}
