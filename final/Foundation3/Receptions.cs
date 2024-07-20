450f4b77396db00a2548876bd7d3a7c0c1bb409e
TyHulet <tyhulet@byui.edu>
2024-07-20 12:39:52 -0600

    added in and finshed the recprion section

--- final/Foundation3/Receptions.cs
+++ final/Foundation3/Receptions.cs
@@ -1,4 +1,15 @@
-public class Receptions
+public class Receptions : Events
 {
+    private string _emailRSVP;
 
+    public Receptions(string eventTitle, string eventDesctiption, string eventDate, string eventTime, string eventAddress, string emailRSVP)
+    : base(eventTitle, eventDesctiption, eventDate, eventTime, eventAddress)
+    {
+        _emailRSVP = emailRSVP;
+    }
+
+    public void DisplayRecptions()
+    {
+        Console.WriteLine($"Email Rsvp: {_emailRSVP}");
+    }
 }
