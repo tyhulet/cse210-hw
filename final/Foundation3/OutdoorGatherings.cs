d31d671fdbf15b5c00fe13eeaeb6c5a68837ad9e
TyHulet <tyhulet@byui.edu>
2024-07-20 12:39:32 -0600

    added and finshed the outdoorGatherings section

--- final/Foundation3/OutdoorGatherings.cs
+++ final/Foundation3/OutdoorGatherings.cs
@@ -1,4 +1,15 @@
-public class OurdoorGaterings
+public class OutdoorGatherings : Events
 {
+    private string _weatherType;
 
+    public OutdoorGatherings(string eventTitle, string eventDesctiption, string eventDate, string eventTime, string eventAddress, string weatherType)
+    : base(eventTitle, eventDesctiption, eventDate, eventTime, eventAddress)
+    {
+        _weatherType = weatherType;
+    }
+
+    public void DisplayWeather()
+    {
+        Console.WriteLine($"Events Weather: {_weatherType}");
+    }
 }
