095222b5769606db9ff19f3c4d1803c3917034cd
TyHulet <tyhulet@byui.edu>
2024-07-20 12:39:08 -0600

    added and finshed the code for the lectures section

--- final/Foundation3/Lectures.cs
+++ final/Foundation3/Lectures.cs
@@ -1,4 +1,19 @@
-public class Lectures
+public class Lectures : Events
 {
+    private string _speaker;
+
+    private int _maxCapicty;
+
+    public Lectures(string eventTitle, string eventDesctiption, string eventDate, string eventTime, string eventAddress, string speaker, int maxCapicty)
+    : base(eventTitle, eventDesctiption, eventDate, eventTime, eventAddress)
+    {
+        _speaker = speaker;
+        _maxCapicty = maxCapicty;
+    }
+
+    public void DisplayLectures()
+    {
+        Console.WriteLine($"Speaker: {_speaker}, Capicty {_maxCapicty}");
+    }
 
 }
