d4bc2ac9f46ddd4dee27d81a14400ca95fa47043
TyHulet <tyhulet@byui.edu>
2024-07-20 21:17:32 -0600

    Fixed the date configuration

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -7,13 +7,13 @@ class Program
     static void Main(string[] args)
     {
         List<Actvitys> activities = new List<Actvitys>();
-        SwimmingActvity swimming = new SwimmingActvity("Aug 1st 2024", 30, 30);
+        SwimmingActvity swimming = new SwimmingActvity("1st July, 2024", 30, 50);
         activities.Add(swimming);
 
-        RunningActvity running = new RunningActvity("July 4th, 2024", 120, 13.7);
+        RunningActvity running = new RunningActvity("4th July, 2024", 120, 13.7);
         activities.Add(running);
 
-        CyclingActvity cycling = new CyclingActvity("July 15th, 2024", 90, 12.2);
+        CyclingActvity cycling = new CyclingActvity("15th July, 2024", 90, 12.2);
         activities.Add(cycling);
 
         foreach (Actvitys actvity in activities)
