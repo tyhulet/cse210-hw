78d64a1751980dbd74505f798d6e5f228770bb90
TyHulet <tyhulet@byui.edu>
2024-07-20 20:00:29 -0600

    add in the foreach loop to go through the different actvitys

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,20 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
+using System.Security.AccessControl;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Actvitys> activities = new List<Actvitys>();
+        SwimmingActvity swimming = new SwimmingActvity("Aug 1st, 2023", 30, 30);
+        activities.Add(swimming);
+
+        foreach (Actvitys actvity in activities)
+        {
+            Console.WriteLine();
+            actvity.ActvitySummary();
+        }
+
     }
 }
